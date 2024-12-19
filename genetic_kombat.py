67fe522f2a1d34ec35a138cd199f9a0abaddec94
Umar Tariq <134786490+Umar-604@users.noreply.github.com>
2024-12-19 22:08:30 +0500

    Update genetic_kombat.py
    Added colors of the players

--- genetic_kombat.py
+++ genetic_kombat.py
@@ -8,3 +8,9 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Genetic Kombat with Genetic Algorithm")
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
